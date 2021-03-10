3cac676d41fb161ec7c470e2f5102cb1df45e0f4
GS-SHENYANG <27382901@qq.com>
2021-03-10 21:14:26 +0800

    master

--- fio_1.bash
+++ fio_1.bash
@@ -2,7 +2,7 @@
 
 
 findfioprocess=$(ps -fe|grep 'fio'|awk '{print $8}')
-while  [ "$findfioprocess" != "fio" ]
+while   [ "$findfioprocess" != "fio" ]
 do  
  cd /data/
  for i in $(seq 80); 
@@ -10,14 +10,16 @@ do  
  fio -filename=/data/$name  -iodepth=64 -ioengine=libaio -direct=1 -rw=write -bs=16m -size=256g -numjobs=1 -group_reporting -name=test-write
  done
 
+
  sleep 1
+
 
 if [ "$findfioprocess" != "fio" ]
 then
 cd /data/
 for i in $(seq 80); 
 do name=$(printf test%02d.txt $i);
- fio -filename=/data/$name  -iodepth=64 -ioengine=libaio -direct=1 -rw=write -bs=16m -size=256g -numjobs=1 -group_reporting -name=test-write
+fio -filename=/data/$name  -iodepth=64 -ioengine=libaio -direct=1 -rw=write -bs=16m -size=256g -numjobs=1 -group_reporting -name=test-write
 done
 
 
